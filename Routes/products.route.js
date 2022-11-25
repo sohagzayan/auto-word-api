ac7078f92346dac12a513ec0fa20a6f9377e17ee
sohag.zayan <gsohag360@gmail.com>
2022-11-25 06:44:18 -0800

    delete slash missing

--- Routes/products.route.js
+++ Routes/products.route.js
@@ -5,6 +5,6 @@ const controller = require("../Controllers/product.controller");
 router.get("/product", controller.getAllProduct);
 router.post("/product", controller.addNewProduct);
 router.get("/product/:id", controller.getSingleProduct);
-router.delete("/product:id", controller.deleteProduct);
+router.delete("/product/:id", controller.deleteProduct);
 
 module.exports = router;
